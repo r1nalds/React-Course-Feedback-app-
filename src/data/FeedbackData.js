195b6b652b7c98efcdaa8cfdba2a483188d7f9ec
Andis Rinalds Eglītis <andis.r.eglitis@gmail.com>
2022-05-05 18:00:35 +0300

    initial

---
+++ src/data/FeedbackData.js
@@ -0,0 +1,19 @@
+const FeedbackData = [
+  {
+    id: 1,
+    rating: 10,
+    text: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quae illum rerum repellat, explicabo, dolorum eius voluptas impedit ipsa, dicta odit perferendis aliquam nihil voluptate voluptatibus cum ullam ea tempore repudiandae?",
+  },
+  {
+    id: 2,
+    rating: 5,
+    text: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quae illum rerum repellat, explicabo, dolorum eius voluptas impedit ipsa, dicta odit perferendis aliquam nihil voluptate voluptatibus cum ullam ea tempore repudiandae?",
+  },
+  {
+    id: 3,
+    rating: 3,
+    text: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quae illum rerum repellat, explicabo, dolorum eius voluptas impedit ipsa, dicta odit perferendis aliquam nihil voluptate voluptatibus cum ullam ea tempore repudiandae?",
+  },
+];
+
+export default FeedbackData;
